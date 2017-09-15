efa56faac6002589e35df3ce2288b37f1a0ae4a3
Tomasz Rosiek <tomasz.rosiek@digital.hmrc.gov.uk>
2017-09-15 11:28:58 +0100

    Use newer version of boostrap library and hmrc test

--- project/FrontendBuild.scala
+++ project/FrontendBuild.scala
@@ -13,13 +13,13 @@ object FrontendBuild extends Build with MicroService {
 
   val compile = Seq(
     ws,
-    "uk.gov.hmrc" %% "bootstrap-play-25" % "0.4.0",
+    "uk.gov.hmrc" %% "bootstrap-play-25" % "0.6.0",
     "uk.gov.hmrc" %% "play-ui" % "7.4.0",
     "uk.gov.hmrc" %% "govuk-template" % "5.3.0"
   )
 
   def test(scope: String = "test") = Seq(
-    "uk.gov.hmrc" %% "hmrctest" % "2.3.0" % scope,
+    "uk.gov.hmrc" %% "hmrctest" % "2.4.0" % scope,
     "org.scalatest" %% "scalatest" % "2.2.6" % scope,
     "org.pegdown" % "pegdown" % "1.6.0" % scope,
     "org.jsoup" % "jsoup" % "1.8.1" % scope,
